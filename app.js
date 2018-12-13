f5f924c7a2504ad5d9c0cfee610c66989fb6b17b
Mark S <markscicluna@hotmail.com>
2018-12-13 17:14:46 +0000

    score now changes according to value

--- app.js
+++ app.js
@@ -2,15 +2,32 @@ $(() => {
   console.log('JS loaded');
   const $submit = $('.submit');
   const $loser = $('.loser');
+  const $winner = $('.winner');
+  const $score = $('.score');
+
+  let $markvmike = $('.markvmike');
 
   $submit.on('click', (e) => {
     e.preventDefault();
-    console.log('hello');
+    console.log('submit clicked');
+    const amount = parseInt($score.val());
+    console.log(amount);
+    let currentScore = parseInt($markvmike.text());
+    $markvmike.text(currentScore + amount);
+
   });
 
+
+
+
   $loser.change(function() {
-    const loser = $(this).find('option:selected');
+    const loser = $(this).find('option:selected').attr('id');
     console.log(loser);
+  });
+
+  $winner.change(function() {
+    const winner = $(this).find('option:selected').attr('id');
+    console.log(winner);
   });
 
 });
